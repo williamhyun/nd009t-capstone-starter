dcd7bdcf506b520453d515a03772c078061f2cfc
William Hyun <william@apache.org>
2022-05-31 05:21:06 +0000

    Add deploy

---
+++ submission/model/code/inference.py
@@ -0,0 +1,45 @@
+import json
+import logging
+import sys
+import os
+import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+import io
+import requests
+
+def model_fn(model_dir):
+    model = timm.create_model('efficientnet_b0', pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+
+    layer = nn.Sequential(
+        nn.BatchNorm1d(model.get_classifier().in_features),
+        nn.Linear(model.get_classifier().in_features, 512, bias=False),
+        nn.ReLU(),
+        nn.BatchNorm1d(512),
+        nn.Dropout(p=0.5, inplace=True),
+        nn.Linear(512, 7, bias=False))
+    model.classifier = layer
+    
+    with open('/opt/ml/model/model/model.pth', 'rb') as f:
+        model.load_state_dict(torch.load(f))
+    model.eval()
+    return model
+
+def input_fn(request_body, content_type):
+    if content_type == 'image/jpeg':
+        return Image.open(io.BytesIO(request_body))
+    raise Exception('Requested unsupported ContentType in content_type: {}'.format(content_type))
+
+def predict_fn(input_object, model):
+    transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
+    input_object=transform(input_object)
+    
+    with torch.no_grad():
+        prediction = model(input_object.unsqueeze(0))
+    return prediction
